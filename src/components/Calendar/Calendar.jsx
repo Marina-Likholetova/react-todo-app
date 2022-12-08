2bca1893fd8d79c51b79d219e7a7d6b86ce3a8f2
Marina <likholetova.marina@gmail.com>
2022-12-08 22:20:22 +0200

    Intagrate Calendar class component

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,78 +1,86 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setDate } from "../../features/date/dateSlice";
-import { getWeek, getPrevWeek, getNextWeek, convertIntoString, DAYS } from "../../utils/calendar/calendar.js";
+import { connect } from "react-redux";
 import { Button, IconButton } from "@mui/material";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import { useState } from "react";
-import './Calendar.css'
+import { setDate } from "../../features/date/dateSlice";
+import { getWeek, getPrevWeek, getNextWeek, convertIntoString, DAYS } from "../../utils/calendar/calendar.js";
+import generateRandomNumber from "../../utils/helpers/generateRandomNumber";
+import "./Calendar.css";
 
 
-
-
-export default function Calendar() {
-    const date = useSelector(state => state.date.value);
-    const dispatch = useDispatch();
-
-    const [week, setWeek] = useState(getWeek(date));
- 
-    const onMoveLeft = () => {
-        setWeek(getPrevWeek(week[0].time));
-    };
-
-    const onMoveRight = () => {
-        setWeek(getNextWeek(week[0].time));
-    };
-
-    const onPickDate = (date) => {
-        dispatch(setDate({ date }));
+class Calendar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            week: getWeek(this.props.date),
+        };
     }
 
- 
+    onMoveLeft = () => {
+        this.setState({ week: getPrevWeek(this.state.week[0].time) });
+    };
 
-    return (
-        <div className="calendar">
-            <h2 className="calendar-title">
-                {convertIntoString(week[0].year, week[0].month)}
-            </h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th></th>
-                        {DAYS.map((day) => (
-                            <th>{day}</th>
-                        ))}
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <IconButton size="small" onClick={onMoveLeft}>
-                                <NavigateBeforeIcon />
-                            </IconButton>
-                        </td>
-                        {week.map((day) => (
+    onMoveRight = () => {
+        this.setState({ week: getNextWeek(this.state.week[0].time) });
+    };
+
+    onPickDate = (date) => {
+        this.props.setDate({ date });
+    };
+
+    render() {
+        const { week } = this.state;
+
+        return (
+            <div className="calendar">
+                <h2 className="calendar-title">{convertIntoString(week[0].year, week[0].month)}</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            {DAYS.map((day) => (
+                                <th key={generateRandomNumber()}>{day}</th>
+                            ))}
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
                             <td>
-                                <Button
-                                    size="small"
-                                    color={day.time === date ? "secondary" : "primary"}
-                                    variant="contained"
-                                    onClick={() => onPickDate(day.time)}
-                                >
-                                    {day.date}
-                                </Button>
+                                <IconButton size="small" onClick={this.onMoveLeft}>
+                                    <NavigateBeforeIcon />
+                                </IconButton>
                             </td>
-                        ))}
-                        <td>
-                            <IconButton size="small" onClick={onMoveRight}>
-                                <NavigateNextIcon />
-                            </IconButton>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    );
+                            {week.map((day) => (
+                                <td key={generateRandomNumber()}>
+                                    <Button
+                                        size="small"
+                                        color={day.time === this.props.date ? "secondary" : "primary"}
+                                        variant="contained"
+                                        onClick={() => this.onPickDate(day.time)}
+                                    >
+                                        {day.date}
+                                    </Button>
+                                </td>
+                            ))}
+                            <td>
+                                <IconButton size="small" onClick={this.onMoveRight}>
+                                    <NavigateNextIcon />
+                                </IconButton>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
+
+const mapStateToProps = (state) => ({
+    date: state.date.value,
+});
+
+const mapDispatchToProps = { setDate };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Calendar);
